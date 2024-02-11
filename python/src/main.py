4cdf449f419b988ef7e5b3f4128513700bbf2722
bestswlkh0310 <hhhello0507@gmail.com>
2024-02-11 19:05:25 +0900

    Revert "refactor"
    This reverts commit da0a9c15159a60fdf2f082f5735110e340fe53cc.

--- python/src/main.py
+++ python/src/main.py
@@ -1,57 +1,51 @@
-from datetime import datetime, timedelta
-import os
-
 import cv2
 import numpy as np
-
+from datetime import datetime, timedelta
+import asyncio
 from src.ai.ai import SkyAI
 from src.model.store import Store
 from src.util.arduino.arduino import SkyArduino
 
-from src.view.main_view import MainView
+np.set_printoptions(suppress=True)
+skyAI = SkyAI(model_path="../res/model/g_model.h5")
+class_names = open("../labels.txt", "r").readlines()
+store = Store(class_names=class_names)
+
+camera = cv2.VideoCapture(1)
+skyArduino = SkyArduino()
 
 
-def predict():
-    _, image = camera.read()
+async def predict():
+    while True:
+        _, image = camera.read()
 
-    skyAI.predict(image=image, output_store=store)
-    cv2.waitKey(1)
+        skyAI.predict(image=image, output_store=store)
+        cv2.waitKey(1)
 
-
-def sendArduino():
-    global before_time
-    current_time = datetime.now()
-    if before_time + timedelta(seconds=1) < current_time:
-        mxIdx = store.get_max_index()
-        skyArduino.send_classification_result(index=mxIdx)
-
-        print(f"sended - mxIdx: {mxIdx}, encoded: {str(mxIdx).encode()}")
-
-        before_time = current_time
-        store.init_predict_store()
+        await asyncio.sleep(0.01)
 
 
-def view():
-    main_view = MainView(title='AI')
-    main_view.init_view()
-    root = main_view.get_root()
-    root.after(10, predict)
-    root.after(10, sendArduino)
-    root.mainloop()
+async def sendArduino():
+    before_time = datetime.now()
+    while True:
+        current_time = datetime.now()
+        if before_time + timedelta(seconds=1) < current_time:
+            mxIdx = store.get_max_index()
+            skyArduino.send_classification_result(index=mxIdx)
+
+            print(f"sended - mxIdx: {mxIdx}, encoded: {str(mxIdx).encode()}")
+
+            before_time = current_time
+            store.init_predict_store()
+        await asyncio.sleep(0.01)
+
+
+async def main():
+    await asyncio.gather(predict(), sendArduino())
 
 
 if __name__ == "__main__":
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    np.set_printoptions(suppress=True)
-    skyAI = SkyAI(model_path="../res/model/g_model.h5")
-    class_names = open("../labels.txt", "r").readlines()
-    store = Store(class_names=class_names)
+    asyncio.run(main())
 
-    camera = cv2.VideoCapture(1)
-    skyArduino = SkyArduino()
-    before_time = datetime.now()
-
-    view()
-    # asyncio.run(ai())
     camera.release()
     cv2.destroyAllWindows()
